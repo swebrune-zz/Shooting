621d0d6cf5c6f173dd8caf6faba7a089ed76e666
swebrune <sweb.rune@gmail.com>
2018-03-06 17:31:53 +0900

    Bullet.cs 銃弾速度設定、Rigidbody2D設定

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed = 5;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+    }
+}
