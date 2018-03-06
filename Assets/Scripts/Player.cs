e11e7f6059e7e7e1e3c44dea86d24bc691ffa0fa
swebrune <sweb.rune@gmail.com>
2018-03-06 17:31:13 +0900

    Player.cs コルーチン追加、銃弾Prefab発射、移動に関する実装

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    // 移動スピード
+    public float speed = 5;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // PlayerBulletプレハブ
+    public GameObject bullet;
+
+    // Startメソッドをコルーチンとして呼び出す
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            // 0.10秒待つ
+            yield return new WaitForSeconds(0.10f);
+        }
+    }
+
+    void Update()
+    {
+        // 右・左
+        float x = Input.GetAxisRaw("Horizontal");
+
+        // 上・下
+        float y = Input.GetAxisRaw("Vertical");
+
+        // 移動する向きを求める
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        // 移動する向きとスピードを代入する
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
 }
